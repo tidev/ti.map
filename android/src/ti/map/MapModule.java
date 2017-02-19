23613008c86457c3875bafdb58d8f4b2990d4aa7
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-02-19 12:59:09 +0100

    Fix float-value

--- android/src/ti/map/MapModule.java
+++ android/src/ti/map/MapModule.java
@@ -85,7 +85,7 @@ public class MapModule extends KrollModule
 	@Kroll.constant public static final float ANNOTATION_ROSE = BitmapDescriptorFactory.HUE_ROSE;
 	@Kroll.constant public static final float ANNOTATION_VIOLET = BitmapDescriptorFactory.HUE_VIOLET;
 	@Kroll.constant public static final float ANNOTATION_YELLOW = BitmapDescriptorFactory.HUE_YELLOW;
-	@Kroll.constant public static final float ANNOTATION_PURPLE = 276.92; // Based on the HUI color scheme
+	@Kroll.constant public static final float ANNOTATION_PURPLE = 276.92f; // Based on the HUI color scheme
 
 	public MapModule()
 	{
