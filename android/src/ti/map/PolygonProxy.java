6c0df551ac7d2046623eda84aca4b386a4f39a51
m1ga <migamiga@web.de>
2019-01-12 13:28:32 +0100

    fix compile for 7.5.0

--- android/src/ti/map/PolygonProxy.java
+++ android/src/ti/map/PolygonProxy.java
@@ -29,7 +29,7 @@ import com.google.android.gms.maps.model.PolygonOptions;
 				 {
 
 					 MapModule.PROPERTY_FILL_COLOR, MapModule.PROPERTY_STROKE_COLOR, MapModule.PROPERTY_STROKE_WIDTH,
-					 MapModule.PROPERTY_ZINDEX, MapModule.PROPERTY_POINTS, PolygonProxy.PROPERTY_HOLES,
+					 MapModule.PROPERTY_ZINDEX, MapModule.PROPERTY_POINTS,
 					 TiC.PROPERTY_TOUCH_ENABLED
 
 				 })
