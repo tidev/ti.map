1f87aaf4d53c95874536695ad69e0bc43a1f6267
Chris Williams <chris.a.williams@gmail.com>
2019-08-30 13:31:30 -0400

    build: use sdk 8.1.1GA

--- test/unit/karma.unit.config.js
+++ test/unit/karma.unit.config.js
@@ -12,7 +12,7 @@ module.exports = config => {
 			'karma-*'
 		],
 		titanium: {
-			sdkVersion: config.sdkVersion || '7.5.2.GA'
+			sdkVersion: config.sdkVersion || '8.1.1.GA'
 		},
 		customLaunchers: {
 			android: {
