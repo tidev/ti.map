87bde4217e1c071bd7e40f067c802f318a3b5232
Vikas Goyal <38314488+vikas-goyal-syncplicity@users.noreply.github.com>
2020-04-21 15:44:07 +0530

    feat(ios): support multiple annotation update

--- ios/Classes/TiMapView.h
+++ ios/Classes/TiMapView.h
@@ -105,5 +105,6 @@
 #pragma mark Framework
 - (void)refreshAnnotation:(TiMapAnnotationProxy *)proxy readd:(BOOL)yn;
 - (void)fireClickEvent:(MKAnnotationView *)pinview source:(NSString *)source deselected:(BOOL)deselected;
+- (void)refreshCoordinateChanges:(TiMapAnnotationProxy *)proxy afterRemove:(void (^)())callBack;
 
 @end
